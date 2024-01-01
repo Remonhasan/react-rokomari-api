b0fbea58dddebc03c234740505d8423851f7bad1
remonhasanapu <remon.hasan@technovista.com.bd>
2024-01-01 15:08:43 +0600

    added login and logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Firebase\JWT\JWT;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -23,5 +25,34 @@ class UserController extends Controller
     {
         $users = User::all();
         return $users;
+    }
+
+    public function login(Request $request)
+    {
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        // Your login logic here to authenticate the user
+        $user = User::where('email', $email)->first();
+
+        if ($user && Hash::check($password, $user->password)) {
+            // User authenticated successfully
+
+            // Generate JWT
+            $key = base64_encode(random_bytes(32)); // 32 bytes (256 bits) for HS256 algorithm
+            
+            $payload = [
+                'user_id' => $user->id,
+                'email' => $user->email,
+            ];
+
+            $algorithm = 'HS256'; // desired algorithm (e.g., HS256)
+
+            $token = JWT::encode($payload, $key, $algorithm);
+
+            return response()->json(['token' => $token], 200);
+        }
+
+        return response()->json(['message' => 'Unauthorized'], 401);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 Route::post('/register', [UserController::class, 'register']);
 Route::get('/users', [UserController::class, 'allUser']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout']);
